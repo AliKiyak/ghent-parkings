f4ef4809c59081b25ce605ca6665cbddee7c951d
Ali Kiyak <alikiyak@outlook.com>
2020-10-14 11:47:53 +0200

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div className='row'>
+            <div className='col-12'>
+                <h1 className='text-center'>Ghent Parkings</h1>
+            </div>
+        </div>
+    )
+}
